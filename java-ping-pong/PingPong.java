e8e752a3ab341717b2c9ecda431a68805c31314b
bernhard <bernhard.denner@gmail.com>
2024-10-11 14:27:53 +0200

    java-pp: do mem allocation on server
    don't perform heavy computational tasks

--- java-ping-pong/PingPong.java
+++ java-ping-pong/PingPong.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.*;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.*;
 import java.util.logging.*;
 
@@ -9,6 +11,11 @@ public class PingPong {
     private static final Logger logger = Logger.getLogger(PingPong.class.getName());
 
     private double highestDuration = 0;
+
+    public static final int OBJECT_SIZE = 1024 * 1024; // 1 MB
+
+    private final List<LargeObject> largeObjects = new LinkedList<>();
+
 
     static {
         // Set up the logger with a custom format
@@ -52,6 +59,14 @@ public class PingPong {
         }
     }
 
+    private class LargeObject {
+        private byte[] data;
+
+        public LargeObject(int size) {
+            data = new byte[size];
+        }
+    }
+
     protected void server() {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             logger.info(String.format("Server is listening on port %d", PORT));
@@ -59,41 +74,66 @@ public class PingPong {
 
             while (true) {
                 Socket socket = serverSocket.accept();
-                executor.submit(() -> handleClient(socket));
+                executor.submit(() -> this.handleClient(socket));
             }
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Server exception", e);
         }
     }
 
-    private static void handleClient(Socket socket) {
+    private boolean heapUsageCritical() {
+        Runtime runtime = Runtime.getRuntime();
+        long maxMemory = runtime.maxMemory();
+        long totalMemory = runtime.totalMemory();
+        long freeMemory = runtime.freeMemory();
+        long usedMemory = totalMemory - freeMemory;
+        long availableMemory = maxMemory - usedMemory;
+
+        double usedMemoryPercentage = (double) usedMemory / maxMemory * 100;
+        double availableMemoryPercentage = (double) availableMemory / maxMemory * 100;
+
+        if (usedMemoryPercentage > 95) {
+            logger.warning(String.format("Used memory: %.2f%%, Available memory: %.2f%%", usedMemoryPercentage,
+                    availableMemoryPercentage));
+            return true;
+        }
+
+        return false;
+    }
+
+    synchronized private void removeLargeObjectsIfRequred() {
+        if (heapUsageCritical()) {
+            logger.warning("Heap usage is critical, removing large objects");
+            for (int i = 0; i < 1000; i++) {
+                if (largeObjects.isEmpty()) {
+                    break;
+                }
+                largeObjects.remove(0);
+            }
+        }
+    }
+
+    private void handleClient(Socket socket) {
         try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
 
             String message;
             while ((message = in.readLine()) != null) {
                 if (message.equals("ping")) {
+
+                    LargeObject largeObject = new LargeObject(OBJECT_SIZE);
+                    removeLargeObjectsIfRequred();
+                    largeObjects.add(largeObject);
+
                     out.println("pong");
 
-                    // Here we are generating some random objects on the heap to put the server
-                    // under stress. We want to trigger memory swap in and swap out of the OS
-                    // so see the delayed effect of this on the round-trip time.
-                    java.security.MessageDigest digest = java.security.MessageDigest.getInstance("SHA-256");
-                    for (int i = 0; i < 100000; i++) {
-                        String randomString = new java.math.BigInteger(130, new java.security.SecureRandom()).toString(32);
-                        digest.update(randomString.getBytes(java.nio.charset.StandardCharsets.UTF_8));
-                    }
-                    String hashString = java.util.Base64.getEncoder().encodeToString(digest.digest());
-                    // include the hashString in the log message to avoid the compiler optimizing
-                    logger.info("Got ping, sent pong " + hashString);
+                    logger.info("Got ping, sent pong, largeobjects count: " + largeObjects.size());
                 } else {
                     logger.warning("Got unknown message: " + message);
                 }
             }
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Client handler exception", e);
-        } catch (java.security.NoSuchAlgorithmException e) {
-            logger.log(Level.SEVERE, "Failed to generate random string", e);
         }
     }
 
@@ -124,7 +164,7 @@ public class PingPong {
                 }
             };
 
-            scheduler.scheduleAtFixedRate(pingTask, 0, 500, TimeUnit.MILLISECONDS);
+            scheduler.scheduleAtFixedRate(pingTask, 0, 250, TimeUnit.MILLISECONDS);
 
             // Keep the client running
             Thread.currentThread().join();
